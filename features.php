9de8c8ed114109baae933187108b47fd91fab44f
Carlo0417 <201980120@psu.palawan.edu.ph>
2022-02-25 05:18:02 +0800

    inc directory done

--- features.php
+++ features.php
@@ -1,22 +1,22 @@
-<?php require_once 'inc/header.php'; ?>
+<?php include 'inc/header.php'; ?>
 
     <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
       <header class="masthead mb-auto">
         <div class="inner">
           <h3 class="masthead-brand">Cover</h3>
           <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="index.php">Home</a>
-            <a class="nav-link" href="features.php">Features</a>
+            <a class="nav-link" href="index.php">Home</a>
+            <a class="nav-link active" href="features.php">Features</a>
             <a class="nav-link" href="contact.php">Contact</a>
           </nav>
         </div>
       </header>
 
       <main role="main" class="inner cover">
-        <h1 class="cover-heading">Welcome to Cover Homepage</h1>
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <h1 class="cover-heading">Features for Cover</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <p class="lead">
           <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
         </p>
       </main>
-<?php require_once 'inc/footer.php'; ?>
+<?php include 'inc/footer.php'; ?>
